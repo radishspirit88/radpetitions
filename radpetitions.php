cab26f7163bfc68ea6ee042e300c260b6b1576c6
Radish Spirit <radishspirit88@gmail.com>
2017-05-11 13:37:32 -0600

    Added activation hook
    Set up custom “petition” post type.

--- radpetitions.php
+++ radpetitions.php
@@ -9,3 +9,20 @@ Author URI:  https://github.com/radishspirit88
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
+
+function radpets_setup_post_type()
+{
+    // register the "petition" custom post type
+    register_post_type( 'petition', ['public' => 'true'] );
+}
+add_action( 'init', 'radpets_setup_post_type' );
+
+function radpets_install()
+{
+    // trigger our function that registers the custom post type
+    radpets_setup_post_type();
+
+    // clear the permalinks after the post type has been registered
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'radpets_install' );
