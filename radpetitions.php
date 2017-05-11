41dafb909d9191a724c9da8d11adef9f273f468d
Radish Spirit <radishspirit88@gmail.com>
2017-05-11 13:15:18 -0600

    Plugin header
    Created the plugin header with basic info.

---
+++ radpetitions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Rad Petitions
+Plugin URI:  https://github.com/radishspirit88/radpetitions
+Description: A rad petition system plugin for WordPress that does not require User accounts.
+Version:     1.0.0
+Author:      Radish Spirit
+Author URI:  https://github.com/radishspirit88
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
